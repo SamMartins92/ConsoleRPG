5220ab3b7f6893f684f638393904cb8aa8ff76b1
samuel.martins <samuel.martins@fsstudio.com>
2022-08-30 11:25:41 +0100

    #Initial Commit

---
+++ RPGame/RPGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player MainPlayer = new Player();
+
+            MainPlayer.MainWeapon = new Weapon(MainPlayer);
+
+        }
+    }
+}
