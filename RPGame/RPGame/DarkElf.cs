d986ec6a86e742363a4608782802649b28137a42
SamuelMartins03 <samuel.martins.9203@gmail.com>
2022-09-05 22:57:33 +0100

    Create first unit tests

--- RPGame/RPGame/DarkElf.cs
+++ RPGame/RPGame/DarkElf.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RPGame
 {
-    internal class DarkElf : Enemy
+    public class DarkElf : Enemy
     {
         public DarkElf()
         {
--- RPGame/UnitTests/BasicTests.cs
+++ RPGame/UnitTests/BasicTests.cs
@@ -5,12 +5,28 @@ namespace UnitTests
 {
     public class BasicTests
     {
+        /// <summary>
+        /// 
+        /// </summary>
         [Fact]
         public void FirstWeaponGive10Strenght()
         {
             Player.Instance.PickUpWeapon(Weapon.WeaponType.Axe);
 
             Assert.True(Player.Instance._strength == 10);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void DarkElfsHaveTheRightName()
+        {
+            DarkElf DarkElf = new DarkElf();
+
+            string name = DarkElf._name;
+
+            Assert.Equal("Dark Elf", name);
         }
     }
 }
