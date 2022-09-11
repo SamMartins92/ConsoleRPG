f66a2304df69364879c748f0bb95567c6acf7762
samuel.martins <samuel.martins@fsstudio.com>
2022-09-11 15:45:45 +0100

    Create Unit Tests

---
+++ RPGame/UnitTests/PlayerTests.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using RPGame;
+
+namespace UnitTests
+{
+    public class PlayerTests
+    {
+        //Pick up weapons
+
+        [Fact]
+        public void CanPickUpAxe()
+        {
+            Player.Instance.PickUpWeapon(Weapon.WeaponType.Axe);
+            Assert.Equal(Weapon.WeaponType.Axe, Player.Instance.MainWeapon.weaponType);
+        }
+
+        [Fact]
+        public void CanPickUpSword()
+        {
+            Player.Instance.PickUpWeapon(Weapon.WeaponType.Sword);
+            Assert.Equal(Weapon.WeaponType.Axe, Player.Instance.MainWeapon.weaponType);
+        }
+
+        [Fact]
+        public void CanPickUpBow()
+        {
+            Player.Instance.PickUpWeapon(Weapon.WeaponType.Bow);
+            Assert.Equal(Weapon.WeaponType.Axe, Player.Instance.MainWeapon.weaponType);
+        }
+
+        [Fact]
+        public void CanPickUpDaggers()
+        {
+            Player.Instance.PickUpWeapon(Weapon.WeaponType.Daggers);
+            Assert.Equal(Weapon.WeaponType.Axe, Player.Instance.MainWeapon.weaponType);
+        }
+
+        [Fact]
+        public void CanMakeDamage()
+        {
+
+        }
+    }
+}
